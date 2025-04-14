7f7352320982a3c3421f8c9a2527b0af4eb1d96b
Alvise Sembenico <alvise.sembenico@gmail.com>
2025-04-14 14:50:39 +0200

    feat: implement modular arithmetic utilities and Benq template

--- lib/modulo/misc.cpp
+++ lib/modulo/misc.cpp
@@ -1,4 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 typedef long long ll;
+
+const int MOD = 1e9 + 7;
 
 // Modular exponentiation
 inline ll modPow(ll x, ll exp, ll mod)
@@ -27,3 +32,39 @@ inline ll mod_mul(ll a, ll b, ll mod)
     }
     return result;
 }
+
+// Benq template
+struct mi
+{
+    ll v;
+    explicit operator int() const { return v; }
+    mi() { v = 0; }
+    mi(long long _v) : v(_v % MOD) { v += (v < 0) * MOD; }
+};
+mi &operator+=(mi &a, mi b)
+{
+    if ((a.v += b.v) >= MOD)
+        a.v -= MOD;
+    return a;
+}
+mi &operator-=(mi &a, mi b)
+{
+    if ((a.v -= b.v) < 0)
+        a.v += MOD;
+    return a;
+}
+mi operator+(mi a, mi b) { return a += b; }
+mi operator-(mi a, mi b) { return a -= b; }
+mi operator*(mi a, mi b) { return mi((long long)a.v * b.v); }
+mi &operator*=(mi &a, mi b) { return a = a * b; }
+mi pow(mi a, long long p)
+{
+    assert(p >= 0);
+    return modPow(a.v, p, MOD);
+}
+mi inv(mi a)
+{
+    assert(a.v != 0);
+    return pow(a, MOD - 2);
+}
+mi operator/(mi a, mi b) { return a * inv(b); }
