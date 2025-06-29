794a67e29e0042aead81ae8dab24f9088996bbee
Alvise Sembenico <alvise.sembenico@gmail.com>
2025-06-29 13:26:29 +0200

    feat: remove unused template functions and add helper functions for grid creation and printing

--- codeforces/base.cpp
+++ codeforces/base.cpp
@@ -18,46 +18,6 @@ typedef vector<ll> vlong;
 #define all(x) x.begin(), x.end()
 #define contains(v, x) (find(begin(v), end(v), x) != end(v))
 
-template <typename T>
-ostream &operator<<(ostream &os, const vector<T> &v)
-{
-    for (int i = 0; i < v.size(); ++i)
-    {
-        os << v[i];
-        if (i != v.size() - 1)
-            os << " ";
-    }
-    return os;
-}
-
-struct custom_hash
-{
-    static uint64_t splitmix64(uint64_t x)
-    {
-        // http://xorshift.di.unimi.it/splitmix64.c
-        x += 0x9e3779b97f4a7c15;
-        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
-        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
-        return x ^ (x >> 31);
-    }
-
-    size_t operator()(uint64_t x) const
-    {
-        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
-        return splitmix64(x + FIXED_RANDOM);
-    }
-};
-
-// function to return a nxm grid of custom types, custom values
-template <typename T>
-vector<vector<T>> createGrid(int n, int m, T defaultValue)
-{
-    return vector<vector<T>>(n, vector<T>(m, defaultValue));
-}
-
-// we can now use it with unordered_map, unordered_set, etc.
-// unordered_map<int, int, custom_hash> m;
-
 int main()
 {
     ios::sync_with_stdio(false);
---
+++ codeforces/helper.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define DEBUG 0
+
+#if DEBUG
+#define HAS_EXTRA 1
+#include "./codeforces/debug.hpp"
+#endif
+
+typedef long long int ll;
+typedef vector<int> vint;
+typedef vector<ll> vlong;
+
+#define loop(a, b) for (int i = a; i < b; i++)
+#define loop0(a) for (int i = 0; i < a; i++)
+#define all(x) x.begin(), x.end()
+#define contains(v, x) (find(begin(v), end(v), x) != end(v))
+
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &v)
+{
+    for (int i = 0; i < v.size(); ++i)
+    {
+        os << v[i];
+        if (i != v.size() - 1)
+            os << " ";
+    }
+    return os;
+}
+
+struct custom_hash
+{
+    static uint64_t splitmix64(uint64_t x)
+    {
+        // http://xorshift.di.unimi.it/splitmix64.c
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const
+    {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+
+// function to return a nxm grid of custom types, custom values
+template <typename T>
+vector<vector<T>> createGrid(int n, int m, T defaultValue)
+{
+    return vector<vector<T>>(n, vector<T>(m, defaultValue));
+}
+
+// we can now use it with unordered_map, unordered_set, etc.
+// unordered_map<int, int, custom_hash> m;
+
+vector<vint> readGrid()
+{
+    int n, m;
+    cin >> n >> m;
+    vector<vint> res(n, vint(m, 0));
+    loop0(n) for (int j = 0; j < m; j++)
+            cin >>
+        res[i][j];
+    return res;
+}
