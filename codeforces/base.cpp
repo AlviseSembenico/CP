d94dfb86b2ab5547dc9243607c4edf5abc1cd3c2
Alvise Sembenico <alvise.sembenico@gmail.com>
2025-07-01 15:35:14 +0200

    feat: implement DSU structure and main logic for road construction problem

--- codeforces/base.cpp
+++ codeforces/base.cpp
@@ -22,17 +22,4 @@ int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-
-    int numTests;
-    cin >> numTests;
-    while (numTests--)
-    {
-        int size;
-        cin >> size;
-        cin.ignore();
-        vector<int> arr(size);
-
-        for (int i = 0; i < size; i++)
-            cin >> arr[i];
-    }
 }
