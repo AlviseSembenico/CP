f7592c96ac371469c84582b675c2791cb5a1cc09
Alvise Sembenico <alvise.sembenico@gmail.com>
2025-08-22 17:23:35 +0200

    feat: add Nearest Smaller Values solution using stack for efficient querying

---
+++ cses/Nearest_Smaller_Values.cpp
@@ -0,0 +1,60 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define DEBUG 0
+
+#if DEBUG
+#define HAS_EXTRA 1
+#include "./codeforces/debug.hpp"
+#endif
+
+#ifndef ONLINE_JUDGE
+#define deb(...) logger(#__VA_ARGS__, __VA_ARGS__)
+template <typename... Args>
+void logger(string vars, Args &&...values)
+{
+    cout << vars << " = ";
+    string delim = "";
+    (..., (cout << delim << values, delim = ", "));
+}
+#else
+// If not debugging, define deb as empty
+#define deb(...)
+#endif
+typedef long long int ll;
+typedef pair<int, int> pii;
+typedef pair<int, ll> pil;
+typedef pair<ll, ll> pll;
+typedef vector<int> vint;
+typedef vector<ll> vlong;
+
+#define loop(a, b) for (int i = a; i < b; i++)
+#define loop0(a) for (int i = 0; i < a; i++)
+#define all(x) x.begin(), x.end()
+#define contains(v, x) (find(begin(v), end(v), x) != end(v))
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    cin >> n;
+    vint v(n);
+    stack<pll> st;
+    loop0(n)
+    {
+        int j;
+        cin >> j;
+        while (!st.empty() && st.top().first >= j)
+            st.pop();
+        if (st.empty())
+            cout << 0 << ' ';
+        else
+            cout << st.top().second << ' ';
+
+        st.push(make_pair(j, i + 1));
+    }
+    return 0;
+}
