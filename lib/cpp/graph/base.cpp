4b5144dcdfb13e260e6e53660c6c1a228abb32d4
Alvise Sembenico <alvise.sembenico@gmail.com>
2025-08-22 17:23:19 +0200

    feat: implement base graph structure with adjacency list representation

---
+++ lib/cpp/graph/base.cpp
@@ -0,0 +1,50 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define DEBUG 0
+
+#if DEBUG
+#define HAS_EXTRA 1
+#include "./codeforces/debug.hpp"
+#endif
+
+#ifndef ONLINE_JUDGE
+#define deb(...) logger(#__VA_ARGS__, __VA_ARGS__)
+template <typename... Args>
+void logger(string vars, Args &&...values)
+{
+    cout << vars << " = ";
+    string delim = "";
+    (..., (cout << delim << values, delim = ", "));
+}
+#else
+// If not debugging, define deb as empty
+#define deb(...)
+#endif
+typedef long long int ll;
+typedef pair<int, int> pii;
+typedef pair<int, ll> pil;
+typedef vector<int> vint;
+typedef vector<ll> vlong;
+
+#define loop(a, b) for (int i = a; i < b; i++)
+#define loop0(a) for (int i = 0; i < a; i++)
+#define all(x) x.begin(), x.end()
+#define contains(v, x) (find(begin(v), end(v), x) != end(v))
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n, m;
+    vector<vector<pii>> g(n);
+    while (m--)
+    {
+        int f, t, w;
+        cin >> f >> t >> w;
+        g[f].push_back(make_pair(t, w));
+        g[t].push_back(make_pair(f, w));
+    }
+}
